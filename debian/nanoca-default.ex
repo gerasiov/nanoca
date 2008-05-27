7b132e0ff33cd61910140b97c429fbca553f1ff1
Alexander GQ Gerasiov <gq@cs.msu.su>
2008-05-28 00:22:00 +0400

    Basic debian dir.
    Signed-off-by: Alexander GQ Gerasiov <gq@cs.msu.su>

---
+++ debian/nanoca-default.ex
@@ -0,0 +1,10 @@
+# Defaults for nanoca initscript
+# sourced by /etc/init.d/nanoca
+# installed at /etc/default/nanoca by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
